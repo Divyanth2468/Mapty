35d8dfb0fd30aea25e574e4d1f7a24d42e32d41c
DivyanthSatya <80347640+Divyanth2468@users.noreply.github.com>
2021-12-19 22:34:50 +0530

    Displaying the map using leaflet API

--- script.js
+++ script.js
@@ -13,11 +13,47 @@ const inputElevation = document.querySelector('.form__input--elevation');
 
 if (navigator.geolocation)
   navigator.geolocation.getCurrentPosition(
+    // Success call back function
     function (position) {
+      // Getting lat and long coordinates from geolocation API and saving them into an array
       const { latitude } = position.coords;
       const { longitude } = position.coords;
-      console.log(latitude, longitude);
+      const coords = [latitude, longitude];
+
+      // Creating the map and storing it inside a map variable
+      const map = L.map('map').setView(coords, 13);
+      // console.log(map);
+
+      // Setting the map style
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      // Adding markers to the map
+      map.on('click', function (mapEvent) {
+        // Getting the coords of the area clicked on the map
+        const { lat, lng } = mapEvent.latlng;
+
+        // Setting up the marker
+        L.marker([lat, lng])
+          .addTo(map)
+          .bindPopup(
+            // Adding custom properties
+            L.popup({
+              maxwidth: 250,
+              minwidth: 100,
+              autoClose: false,
+              closeOnClick: false,
+              className: 'running-popup',
+            })
+          )
+          // Setting the popup content
+          .setPopupContent('running')
+          .openPopup();
+      });
     },
+    // Failure call back function
     function () {
       alert('Could not get your current position');
     }
